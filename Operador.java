9010ed4c1e5d2c50457a625ff5422086ee8753dc
deraldoduran <deraldoduran@gmail.com>
2020-12-05 20:28:18 -0300

    com to string

--- Operador.java
+++ Operador.java
@@ -4,4 +4,8 @@ public class Operador extends Funcionario{
     public Operador (){
         super(n, m, l, s);
     }
+
+    public String toString(){
+        return this.super.nome + ":" + this.super.matricula + ":" + this.super.login + ":" this.super.senha;
+    }
 }
