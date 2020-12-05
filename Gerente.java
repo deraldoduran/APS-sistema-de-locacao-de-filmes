99ee9a4bbc9a954c3ff1927d87b8647469c8179e
deraldoduran <deraldoduran@gmail.com>
2020-12-05 19:58:48 -0300

    quase pronto

--- Gerente.java
+++ Gerente.java
@@ -1,22 +1,31 @@
+import java.util.ArrayList;
+
+import atv02.Produto;
+
 public class Gerente extends Funcionario{
 
-        public Cliente cliente;
-        public Produto produtoQualquer;
-        public Operador operador;
+        private ArrayList <Cliente> cliente;
+        
+        private ArrayList <Produto> produtoQualquer;
+        
+        private ArrayList <Operador> operador;
+
 
     public Gerente(){
         super(n, m, l, s);
     }
 
-    public void adicionaCliente(String cli){
-        this.cliente = cli;
+    public void adicionaCliente(String n, int m, String en, int id, char se ){
+        
+        this.cliente =new Cliente(n, m, en, id, se);
     }
 
-    public void adicionarProdutoQualquer(String p){
-        this.produtoQualquer = p;
+    public void adicionarProdutoQualquer(String c, String t, String g, boolean l){
+        this.produtoQualquer = new Produto(c,t,g,l);
+        
     }
 
-    public void adicionarOperador(String op){
-        this.operador = op;
+    public void adicionarOperador(String n, int m, String l, int s){
+        this.operador = new Operador(n,m,l,s);
     }
 }
