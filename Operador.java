bf01e5b3862dd0678c06d5c316b9bd4903c12ef5
Deraldo Duran <deraldoduran@alu.ufc.br>
2020-12-08 13:39:51 -0300

    alterado

--- Operador.java
+++ Operador.java
@@ -1,11 +1,21 @@
-public class Operador extends Funcionario{
+import java.util.ArrayList;
 
+public class Operador extends Funcionario {
+    private ArrayList <Cliente> cliente;
+        
+    private ArrayList <Produto> produtoQualquer;
 
     public Operador (){
         super(n, m, l, s);
+        this.cliente = new ArrayList<>();
+        this.produtoQualquer = new ArrayList<>();
     }
 
     public String toString(){
         return this.nome + ":" + this.matricula + ":" + this.login + ":" + this.senha;
+    }
+
+    public void fazerLocacao(){
+        
     }
 }
