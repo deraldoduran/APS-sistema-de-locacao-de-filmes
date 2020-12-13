792759fac9603e8822577e5c2e4f759d27b51cdb
difeitoza <diegofeitozabr@gmail.com>
2020-12-13 12:01:13 -0300

    correcoes + adc toString + boolean arranhado

--- src/main/java/br/com/locamax/api/produto/filme/BluRay.java
+++ src/main/java/br/com/locamax/api/produto/filme/BluRay.java
@@ -2,10 +2,23 @@ package main.java.br.com.locamax.api.produto.filme;
 
 public class BluRay extends Filmes {
     public String idiomas;
+    public Boolean arranhado;
     
-    public BluRay(String c, String t, String g, boolean l, int ano, int du, String idi) {
-        super(c, t, g, l, ano, du);
+    public BluRay(String cod, String tit, String gen, int ano, int dur, String idi, Boolean arr) {
+        super(cod, tit, gen, ano, dur);
+        this.arranhado = arr;
         this.idiomas = idi;
+    }
+
+    public String toString(){
+        return "código: " + this.codigo + "\n" +
+            "título: " + this.titulo + "\n" +
+            "genero: " + this.genero + "\n" +
+            "ano lancamento: " + this.anoLancamento + "\n" +
+            "duracao (min): " + this.duracao + "\n" +
+            "idiomas: " + this.idiomas + "\n" +
+            "aranhado: " + this.arranhado + "\n" +
+            "locado: " + this.locado + "\n";
     }
 
     @Override
