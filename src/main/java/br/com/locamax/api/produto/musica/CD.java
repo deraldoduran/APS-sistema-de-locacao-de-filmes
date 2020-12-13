5c56e7888b87a196a311e5151123bf6f6122980e
difeitoza <diegofeitozabr@gmail.com>
2020-12-13 18:01:29 -0300

    adc metodo toString

--- src/main/java/br/com/locamax/api/produto/musica/CD.java
+++ src/main/java/br/com/locamax/api/produto/musica/CD.java
@@ -3,10 +3,21 @@ package main.java.br.com.locamax.api.produto.musica;
 public class CD extends Musica{
     public boolean arranhado, duplo;
 
-    public CD(String cod, String tit, String gen, String aut, int num, boolean dup, boolean arr) {
-        super(cod, tit, gen, aut, num);
+    public CD(String cod, String tit, String gen, String aut, int numF, boolean dup, boolean arr) {
+        super(cod, tit, gen, aut, numF);
         this.duplo = dup;
         this.arranhado = arr;
+    }
+
+    public String toString(){
+        return "código: " + this.codigo + "\n" +
+            "título: " + this.titulo + "\n" +
+            "genero: " + this.genero + "\n" +
+            "autor: " + this.autor + "\n" +
+            "numero de faixas: " + this.numFaixas + "\n" +
+            "duplo: " + this.duplo + "\n" +
+            "arranhado: " + this.arranhado + "\n" + 
+            "locado: " + this.locado;
     }
 
     @Override
