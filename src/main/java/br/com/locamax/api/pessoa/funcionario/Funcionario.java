b5b9aea6022bfd82fd17b2beb079992947b12a5f
difeitoza <diegofeitozabr@gmail.com>
2020-12-13 11:56:07 -0300

    adc metodos toString, getters e setters

--- src/main/java/br/com/locamax/api/pessoa/funcionario/Funcionario.java
+++ src/main/java/br/com/locamax/api/pessoa/funcionario/Funcionario.java
@@ -1,7 +1,8 @@
 package main.java.br.com.locamax.api.pessoa.funcionario;
+
 import main.java.br.com.locamax.api.pessoa.Pessoa;
 
-public abstract class Funcionario extends Pessoa{
+public class Funcionario extends Pessoa {
     public String login;
     public String senha;
 
@@ -9,5 +10,17 @@ public abstract class Funcionario extends Pessoa{
         super(n, m);
         this.login = l;
         this.senha = s;
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public String toString() {
+        return this.login + " - " + this.senha;
     }
 }
