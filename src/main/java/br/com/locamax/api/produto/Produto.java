cb56c58082977e095b612b9fa1627fed2329c132
difeitoza <diegofeitozabr@gmail.com>
2020-12-13 11:59:16 -0300

    adc getters e setters

--- src/main/java/br/com/locamax/api/produto/Produto.java
+++ src/main/java/br/com/locamax/api/produto/Produto.java
@@ -6,18 +6,53 @@ public abstract class Produto {
     public String genero;
     public boolean locado;
 
-    public Produto(String c, String t, String g, boolean l){
-        this.codigo = c;
-        this.titulo = t;
-        this.genero = g;
-        this.locado = l;
+    public Produto(String cod, String tit, String gen){
+        this.codigo = cod;
+        this.titulo = tit;
+        this.genero = gen;
+        this.locado = false;
     }
 
+    public String toString(){
+        return "código: " + this.codigo + "\n" +
+            "título: " + this.titulo + "\n" +
+            "genero: " + this.genero + "\n" +
+            "locado: " + this.locado + "\n";
+    }
+
+    public String getCodigo() {
+        return codigo;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public String getGenero() {
+        return genero;
+    }
+
+    public Boolean getLocado() {
+        return locado;
+    }
+
+    public void setCodigo(String codigo) {
+        this.codigo = codigo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+    
+    public void setGenero(String genero) {
+        this.genero = genero;
+    }
+
+    public void setLocado(boolean locado) {
+        this.locado = locado;
+    }
+    
     public void quaisProdutos(){}
     
     abstract public double calcularDiaria();
-
-    public String toString(){
-        return this.codigo + ":" + this.titulo + ":" + this.genero + ":" + this.locado;
-    }
 }
