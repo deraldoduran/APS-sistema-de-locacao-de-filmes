1c74c403077d5eb676b3e22dc673f3ecffc1e2b0
difeitoza <diegofeitozabr@gmail.com>
2020-12-13 12:03:47 -0300

    correcoes + adc toString

--- src/main/java/br/com/locamax/api/produto/filme/Filmes.java
+++ src/main/java/br/com/locamax/api/produto/filme/Filmes.java
@@ -2,16 +2,21 @@ package main.java.br.com.locamax.api.produto.filme;
 import main.java.br.com.locamax.api.produto.Produto;
 
 public abstract class Filmes extends Produto{
-    
     public int anoLancamento;
     public int duracao;
     
-    public Filmes(String c, String t, String g, boolean l, int ano, int du) {
-        super(c, t, g, l);
+    public Filmes(String cod, String tit, String gen, int ano, int dur) {
+        super(cod, tit, gen);
         this.anoLancamento = ano;
-        this.duracao = du;
+        this.duracao = dur;
     }
 
-    
-    
+    public String toString(){
+        return "código: " + this.codigo + "\n" +
+            "título: " + this.titulo + "\n" +
+            "genero: " + this.genero + "\n" +
+            "locado: " + this.locado + "\n" +
+            "ano lancamento: " + this.anoLancamento + "\n" +
+            "duracao (min): " + this.duracao + "\n";
+    }
 }
--- src/main/java/br/com/locamax/api/produto/filme/VHS.java
+++ src/main/java/br/com/locamax/api/produto/filme/VHS.java
@@ -3,9 +3,19 @@ package main.java.br.com.locamax.api.produto.filme;
 public class VHS extends Filmes{
     public boolean cores;
 
-    public VHS(String c, String t, String g, boolean l, int ano, int du, boolean cor) {
-        super(c, t, g, l, ano, du);
+    public VHS(String cod, String tit, String gen, int ano, int dur, boolean cor) {
+        super(cod, tit, gen, ano, dur);
         this.cores = cor;
+    }
+
+    public String toString(){
+        return "código: " + this.codigo + "\n" +
+            "título: " + this.titulo + "\n" +
+            "genero: " + this.genero + "\n" +
+            "ano lancamento: " + this.anoLancamento + "\n" +
+            "duracao (min): " + this.duracao + "\n" +
+            "em cores: " + this.cores + "\n" +
+            "locado: " + this.locado + "\n";
     }
 
     @Override
