70660dae08c8808a2ffc206b4bb50033c34aeb47
difeitoza <diegofeitozabr@gmail.com>
2020-12-15 02:02:40 -0300

    shell com input automatico para testes

--- src/main/java/br/com/locamax/api/shell/Shell.java
+++ src/main/java/br/com/locamax/api/shell/Shell.java
@@ -1,22 +1,38 @@
 package main.java.br.com.locamax.api.shell;
 
 import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
 
 public class Shell {
+    BufferedReader buff;
     Scanner sc;
     String op;
+    boolean auto = false; //mude para true caso queira automatizar o shell
 
-    public Shell(){
+    public Shell() throws IOException {
+       this.buff = new BufferedReader(new FileReader("src/main/java/br/com/locamax/api/shell/input.txt"));
        this.sc = new Scanner(System.in);
        this.op = "";
     }
 
-    public String nextLine(){
-        op = sc.nextLine();
-        //ignorando entradas que comecem com '/' para facilitar inputs em lote
-        while(op.subSequence(0, 1).equals("/")){
-            op = sc.nextLine();
+    public String nextLine() throws IOException {
+        if (auto) {
+            op = buff.readLine();
+            if (op != null) { // evita exceção quando arquivo chega ao fim
+                while(op.subSequence(0, 1).equals("/")){ //verifica se a linha é comentário
+                    op = buff.readLine();
+                }
+                System.out.println(op);
+                return op;
+            } else {
+                auto = false; //torna manual quando arquivo lê a última linha
+                buff.close();
+            }
         }
+
+        op = sc.nextLine();
         return op;
     }
 
