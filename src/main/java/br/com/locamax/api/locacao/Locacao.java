76cdbf75183a7701c73ffaab1b320113f6cca216
difeitoza <diegofeitozabr@gmail.com>
2020-12-15 22:40:18 -0300

    logica de locacao em andamento

--- src/main/java/br/com/locamax/api/locacao/Locacao.java
+++ src/main/java/br/com/locamax/api/locacao/Locacao.java
@@ -1,32 +1,39 @@
 package main.java.br.com.locamax.api.locacao;
 
-import main.java.br.com.locamax.api.produto.Produto;
-// import main.java.br.com.locamax.api.pessoa.cliente.Cliente;
+import java.util.Calendar;
+import java.text.DateFormat;
 import java.util.Date;
+// import main.java.br.com.locamax.api.pessoa.cliente.Cliente;
 
-public class Locacao extends Produto {
+public class Locacao {
+    int matricula;
+    String codigoProd;
     Date dataSaida;
     Date dataEntregaPrevista;
     Date dataDevolucao;
 
-    public Locacao(String c, String t, String g, Date ds, Date ep, Date dd){
-        super(c, t, g);
+    public Locacao (String cod, int mat, Date ds, Date dep){
+        this.matricula = mat;
+        this.codigoProd = cod;
         this.dataSaida = ds;
-        this.dataEntregaPrevista = ep;
-        this.dataDevolucao = dd;
+        this.dataEntregaPrevista = dep;
     }
     
-    /* public void fazerLocacao(int m, String c, Date ds, Date ep, Date dd){
-        Cliente cli = new Cliente(m);
-            cli.matricula = m;
-            super.codigo = c;
-            this.dataSaida = ds;
-            this.dataEntregaPrevista = ep;
-            this.dataDevolucao = dd;
-    } */
-
-    @Override
-    public double calcularDiaria(){
-        return 0;
+    public void fazerLocacao(int mat, String cod, Date ds, Date dep){
+        Calendar c = Calendar.getInstance();
+        c.set(2013, Calendar.FEBRUARY, 28);
+        Date data = c.getTime();
+        System.out.println("Data atual sem formatação: " + data);
+        //Formata a data
+        DateFormat formataData = DateFormat.getDateInstance();
+        System.out.println("Data atual com formatação: "+ formataData.format(data));
+        //Formata Hora
+        DateFormat hora = DateFormat.getTimeInstance();
+        System.out.println("Hora formatada: "+hora.format(data));
+        //Formata Data e Hora
+        DateFormat dtHora = DateFormat.getDateTimeInstance();
+        System.out.println(dtHora.format(data));
     }
+    
+    public double calcularDiaria(Date dd){ return 0; }
 }
